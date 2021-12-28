00a7f91d4dc80b7c9f98ef5268a09512dfd50f90
Allan Wasega <allanwasega@gmail.com>
2021-12-28 12:30:48 +0300

    Updated ReadMe doc

--- train_ensemble.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-model="dose"
-for seed in {1..5}; do 
-	python run_benchmark.py --vis --vis-latents --seed $seed --train --benchmark $model --dataset gaussian 
-done
-
-python run_benchmark.py --vis --vis-latents --test --benchmark $model --dataset gaussian
